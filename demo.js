f5f7ba65ed1ff464fc597922a6c9c6fa0a1ae162
anna <779093853@qq.com>
2020-12-10 15:18:41 +0800

    修改test分支内容

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   let box = document.getElementsByClassName('box')[0];
   box.addEventListener('click', () => {
+    console.log('这是test分支内容的代码');
     alert('这是测试代码,修改测试代码');
   })
 }
